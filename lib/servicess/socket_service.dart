a70373e4403c3373cc5eb2e68d369f16a092f12b
akaksh001 <akisamdani@gmail.com>
2026-08-10 21:34:06 +0530

    Remove unused foundation import in SocketService

--- lib/servicess/socket_service.dart
+++ lib/servicess/socket_service.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+
 import 'package:latlong2/latlong.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 import '../modelss/bus.dart';
